be651edf5d81cc15020cffe3e048dd0c25e4d552
AllanHyr <djaleial@outlook.fr>
2024-02-26 04:37:55 +0100

    Initialisation du client lourd

---
+++ ClientLourd/src/router/routes.ts
@@ -0,0 +1,23 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('layouts/PublicLayout.vue'),
+    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+  },
+  {
+    path: '/private',
+    component: () => import('layouts/PrivateLayout.vue'),
+    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+];
+
+export default routes;
